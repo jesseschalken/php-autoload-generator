ca3ac03f978e3021b11e18753d99b1334c5f2123
Jesse Schalken <me@jesseschalken.com>
2015-07-24 19:53:59 +1000

    Fix path to autoload.php when included in another project

--- generate-autoload.php
+++ generate-autoload.php
@@ -3,7 +3,10 @@
 
 namespace AutoloadGenerator;
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (file_exists(__DIR__ . '/vendor/autoload.php'))
+    require_once __DIR__ . '/vendor/autoload.php';
+else
+    require_once __DIR__ . '/../../autoload.php';
 
 /**
  * Returns $path relative to $base
