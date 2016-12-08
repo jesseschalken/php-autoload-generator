db63975ac76aceadfe6742692006c536ee85ef1e
Jesse Schalken <me@jesseschalken.com>
2016-12-08 18:28:45 +1100

    Refactoring

--- main.php
+++ main.php
@@ -1,192 +1,10 @@
 <?php
 
-namespace AutoloadGenerator;
+namespace JesseSchalken\AutoloadGenerator;
 
-class GeneratorOptions {
-    public $requireMethod   = 'require_once';
-    public $prependAutoload = false;
-    public $caseInsensitive = false;
-    public $generatedBy     = '';
-}
-
-class Generator {
-    /** @var string[] */
-    private $map = array();
-    /** @var true[] */
-    private $require = array();
-    /** @var \PhpParser\Parser */
-    private $parser;
-
-    public function __construct() {
-        $this->parser = new \PhpParser\Parser(new \PhpParser\Lexer);
-    }
-
-    /**
-     * @param string $path
-     */
-    public function addFile($path) {
-        $contents = file_get_contents($path);
-
-        // Remove the hash-bang line if there, since
-        // PhpParser doesn't support it
-        if (substr($contents, 0, 2) === '#!') {
-            $contents = substr($contents, strpos($contents, "\n") + 1);
-        }
-
-        $nodes = $this->parser->parse($contents);
-
-        foreach ($nodes as $node) {
-            $this->processNode($path, $node);
-        }
-    }
-
-    /**
-     * @param string          $file
-     * @param \PhpParser\Node $node
-     * @param string          $prefix
-     */
-    private function processNode($file, \PhpParser\Node $node, $prefix = '') {
-        if ($node instanceof \PhpParser\Node\Stmt\Const_ && $node->consts) {
-            $this->require[$file] = true;
-        } else if ($node instanceof \PhpParser\Node\Stmt\Function_) {
-            $this->require[$file] = true;
-        } else if ($node instanceof \PhpParser\Node\Expr\FuncCall) {
-            // Try to catch constants defined with define()
-            if (
-                $node->name instanceof \PhpParser\Node\Name &&
-                $node->name->parts === array('define')
-            ) {
-                $this->require[$file] = true;
-            }
-        } else if ($node instanceof \PhpParser\Node\Stmt\ClassLike) {
-            $this->map[$prefix . $node->name] = $file;
-        }
-
-        if ($node instanceof \PhpParser\Node\Stmt\Namespace_) {
-            /** @var \PhpParser\Node\Name|null $name */
-            $name = $node->name;
-            if ($name && $name->parts) {
-                $prefix2 = join('\\', $name->parts) . '\\';
-            } else {
-                $prefix2 = '';
-            }
-
-            foreach ($node->stmts as $node2) {
-                $this->processNode($file, $node2, $prefix . $prefix2);
-            }
-        } else {
-            // Cheating a little. I really just want to traverse the tree
-            // recursively without needing to write code specifically for
-            // each kind of node. This seems to be what \PhpParser\NodeTraverser
-            // does anyway.
-            foreach ($node->getSubNodeNames() as $name) {
-                $var = $node->$name;
-                if ($var instanceof \PhpParser\Node) {
-                    $this->processNode($file, $var, $prefix);
-                } else if (is_array($var)) {
-                    foreach ($var as $var2) {
-                        if ($var2 instanceof \PhpParser\Node) {
-                            $this->processNode($file, $var2, $prefix);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * @param string           $base
-     * @param GeneratorOptions $options
-     * @return string
-     */
-    private function generateClassAutoload($base, GeneratorOptions $options) {
-        $map = array();
-        foreach ($this->map as $class => $file) {
-            $file = make_relative($file, $base);
-            $file = str_replace(DIRECTORY_SEPARATOR, '/', $file);
-
-            if ($options->caseInsensitive) {
-                $class = strtolower($class);
-            }
-
-            $map[$class] = $file;
-        }
-
-        ksort($map, SORT_STRING);
-        $map = var_export($map, true);
-        $php = <<<s
-spl_autoload_register(function (\$class) {
-    static \$map = $map;
-
-
-s;
-
-        if ($options->caseInsensitive) {
-            $php .= <<<s
-    \$class = strtolower(\$class);
-
-
-s;
-        }
-
-        $prepend = var_export($options->prependAutoload, true);
-
-        $php .= <<<s
-    if (isset(\$map[\$class])) {
-        $options->requireMethod __DIR__ . "/{\$map[\$class]}";
-    }
-}, true, $prepend);
-
-s;
-
-        return $php;
-    }
-
-    /**
-     * @param string           $base
-     * @param GeneratorOptions $options
-     * @return string
-     */
-    private function generateRequiredFiles($base, GeneratorOptions $options) {
-        $php = '';
-
-        $require = array_keys($this->require);
-        $require = array_unique($require);
-        $require = array_map(function ($file) use ($base) {
-            $file = make_relative($file, $base);
-            $file = str_replace(DIRECTORY_SEPARATOR, '/', $file);
-            return $file;
-        }, $require);
-        $require = array_unique($require);
-        sort($require, SORT_STRING);
-        foreach ($require as $file) {
-            $file = var_export("/$file", true);
-            $php .= "$options->requireMethod __DIR__ . $file;\n";
-        }
-
-        return $php;
-    }
-
-    /**
-     * @param string           $base
-     * @param GeneratorOptions $options
-     * @return string
-     */
-    public function generate($base, GeneratorOptions $options) {
-        $autoload = $this->generateClassAutoload($base, $options);
-        $required = $this->generateRequiredFiles($base, $options);
-
-        return <<<s
-<?php
-
-// !! Generated by: $options->generatedBy
-
-$autoload
-$required
-
-s;
-    }
-}
+use PhpParser\Lexer;
+use PhpParser\Node;
+use PhpParser\Parser;
 
 /**
  * Returns $path relative to $base
@@ -195,58 +13,97 @@ s;
  * @return string
  */
 function make_relative($path, $base) {
-    $path  = explode(DIRECTORY_SEPARATOR, realpath($path));
-    $base  = explode(DIRECTORY_SEPARATOR, realpath($base));
-    $count = min(count($path), count($base));
+    $path = \explode(\DIRECTORY_SEPARATOR, \realpath($path));
+    $base = \explode(\DIRECTORY_SEPARATOR, \realpath($base));
+    $count = \min(\count($path), \count($base));
 
     for ($i = 0; $i < $count && $path[$i] === $base[$i]; $i++) {
     }
 
-    $path = array_slice($path, $i);
-    $base = array_slice($base, $i);
+    $path = \array_slice($path, $i);
+    $base = \array_slice($base, $i);
 
-    $path = array_merge(array_fill(0, count($base), '..'), $path);
+    $path = \array_merge(\array_fill(0, \count($base), '..'), $path);
 
-    return implode(DIRECTORY_SEPARATOR, $path);
+    return \implode(\DIRECTORY_SEPARATOR, $path);
 }
 
-/**
- * @param string $path
- * @param bool   $followLinks
- * @return string[]
- */
-function recursive_scan($path, $followLinks = false) {
-    $result = array($path);
+final class ParsedFile {
+    public static function parse($code) {
+        // Remove the hash-bang line if there, since PhpParser doesn't support it
+        if (\substr($code, 0, 2) === '#!') {
+            $code = \substr($code, strpos($code, "\n") + 1);
+        }
 
-    if (is_dir($path) && ($followLinks || !is_link($path))) {
-        foreach (array_diff(scandir($path), array('.', '..')) as $p) {
-            foreach (recursive_scan($path . DIRECTORY_SEPARATOR . $p, $followLinks) as $p2) {
-                $result[] = $p2;
+        $parser = new Parser(new Lexer);
+        $self = new self();
+        foreach ($parser->parse($code) as $node) {
+            $self->processNode($node, '');
+        }
+        return $self;
+    }
+
+    /** @var string[] */
+    private $classes = array();
+    /** @var bool */
+    private $loadEagerly = false;
+
+    /**
+     * @param Node   $node
+     * @param string $prefix
+     */
+    private function processNode(Node $node, $prefix) {
+        if ($node instanceof Node\Stmt\Const_ && $node->consts) {
+            $this->loadEagerly = true;
+        } else if ($node instanceof Node\Stmt\Function_) {
+            $this->loadEagerly = true;
+        } else if ($node instanceof Node\Expr\FuncCall) {
+            // Try to catch constants defined with define()
+            if (
+                $node->name instanceof Node\Name &&
+                $node->name->parts === array('define')
+            ) {
+                $this->loadEagerly = true;
+            }
+        } else if ($node instanceof Node\Stmt\ClassLike) {
+            $this->classes[] = $prefix . $node->name;
+        }
+
+        if ($node instanceof Node\Stmt\Namespace_) {
+            /** @var Node\Name|null $name */
+            $name = $node->name;
+            if ($name && $name->parts) {
+                $prefix = join('\\', $name->parts) . '\\';
+            } else {
+                $prefix = '';
+            }
+        }
+
+        foreach ($node->getSubNodeNames() as $name) {
+            $var = $node->$name;
+            if ($var instanceof Node) {
+                $this->processNode($var, $prefix);
+            } else if (\is_array($var)) {
+                foreach ($var as $var2) {
+                    if ($var2 instanceof Node) {
+                        $this->processNode($var2, $prefix);
+                    }
+                }
             }
         }
     }
 
-    return $result;
-}
-
-/**
- * @param string[] $paths
- * @return string[]
- */
-function flatten_input_paths(array $paths) {
-    $result = array();
-    foreach ($paths as $path) {
-        foreach (recursive_scan(realpath($path)) as $file) {
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-            if ($extension === 'php' || $extension === 'hh') {
-                $result[] = $file;
-            }
-        }
+    public function getClasses() {
+        return $this->classes;
     }
-    return $result;
+
+    public function getLoadEagerly() {
+        return $this->loadEagerly;
+    }
 }
 
-const DOC = <<<'s'
+final class Generator {
+    const DOC = <<<'s'
 A PHP class autoload generator with support for functions and constants.
 
 Example:
@@ -271,33 +128,158 @@ Options:
                                a strtolower() call every time a class is loaded.
     --prepend                  Third parameter to spl_autoload_register().
     --exclude <file>           Exclude a file/directory.
+    --follow-symlinks          Follow symbolic links.
 
 s;
 
-function main(array $argv) {
-    $args = \Docopt::handle(DOC, array('argv' => \array_slice($argv, 1)));
+    public static function main(array $argv) {
+        $args = \Docopt::handle(self::DOC, array('argv' => \array_slice($argv, 1)));
 
-    $outFile = $args['<outfile>'];
-    $files   = array_diff(
-        flatten_input_paths($args['<files>'] ?: array(dirname($outFile))),
-        flatten_input_paths($args['--exclude'])
-    );
+        $self = new self($args['<outfile>']);
+        $self->generatedBy = \join(' ', $argv);
+        $self->followSymlinks = $args['--follow-symlinks'];
+        $self->requireMethod = $args['--require-method'];
+        $self->prependAutoload = $args['--prepend'];
+        $self->caseInsensitive = $args['--case-insensitive'];
 
-    $options                  = new GeneratorOptions;
-    $options->requireMethod   = $args['--require-method'];
-    $options->prependAutoload = $args['--prepend'];
-    $options->caseInsensitive = $args['--case-insensitive'];
-    $options->generatedBy     = join(' ', $argv);
+        $files = $self->flattenInputPaths($args['<files>'] ?: array($self->baseDir));
+        $files = \array_diff($files, $self->flattenInputPaths($args['--exclude']));
 
-    $generator = new Generator;
-
-    foreach ($files as $file) {
-        print "Scanning $file\n";
-        $generator->addFile($file);
+        foreach ($files as $file) {
+            $parsed = ParsedFile::parse(\file_get_contents($file));
+            if ($parsed->getLoadEagerly()) {
+                $self->eagerFiles[] = $file;
+            }
+            foreach ($parsed->getClasses() as $class) {
+                $self->classMap[$class] = $file;
+            }
+            print "Scanning $file\n";
+        }
+        print "\n";
+        \file_put_contents($self->outFile, $self->generate());
+        print "Output written to $self->outFile\n";
     }
-    print "\n";
 
-    file_put_contents($outFile, $generator->generate(dirname($outFile), $options));
-    print "Output written to $outFile\n";
+    /** @var string */
+    private $outFile;
+    /** @var string */
+    private $generatedBy = '';
+    /** @var bool */
+    private $followSymlinks = false;
+    /** @var string */
+    private $baseDir;
+    /** @var string[] */
+    private $classMap = array();
+    /** @var string[] */
+    private $eagerFiles = array();
+    /** @var bool */
+    private $caseInsensitive = false;
+    /** @var bool */
+    private $prependAutoload = false;
+    /** @var string */
+    private $requireMethod = 'require_once';
+
+    private function __construct($outFile) {
+        $this->outFile = $outFile;
+        $this->baseDir = \dirname($outFile);
+    }
+
+    private function generate() {
+        $code = "";
+        $code .= "<?php\n";
+        $code .= "\n";
+        $code .= "// Generated by " . $this->generatedBy . "\n";
+        $code .= "\n";
+        $code .= $this->generateSplAutoload();
+        $code .= "\n";
+        $code .= $this->generateEagerRequires();
+        $code .= "\n";
+        return $code;
+    }
+
+    private function generateEagerRequires() {
+        $self = $this;
+
+        $files = $this->eagerFiles;
+        $files = \array_unique($files);
+        $files = \array_map(function ($file) use ($self) {
+            return $self->cleanPath($file);
+        }, $files);
+        $files = \array_unique($files);
+
+        \sort($files, SORT_STRING);
+
+        $code = '';
+        foreach ($files as $file) {
+            $code .= "$this->requireMethod __DIR__ . " . var_export("/$file", true) . ";\n";
+        }
+        return $code;
+    }
+
+    private function generateSplAutoload() {
+        $self = $this;
+
+        $classMap = $this->classMap;
+        $classMap = \array_map(function ($file) use ($self) {
+            return $self->cleanPath($file);
+        }, $classMap);
+        if ($this->caseInsensitive) {
+            $classMap = \array_change_key_case($classMap, CASE_LOWER);
+        }
+
+        \ksort($classMap, SORT_STRING);
+
+        $code = "";
+        $code .= "\\spl_autoload_register(function (\$class) {\n";
+        $code .= "  static \$map = " . \var_export($classMap, true) . ";\n";
+        $code .= "\n";
+        if ($this->caseInsensitive) {
+            $code .= "  \$class = \\strtolower(\$class);\n";
+        }
+        $code .= "  if (isset(\$map[\$class])) {\n";
+        $code .= "    $this->requireMethod __DIR__ . '/' . \$map[\$class];\n";
+        $code .= "  }\n";
+        $code .= "}, true, " . ($this->prependAutoload ? 'true' : 'false') . ");\n";
+        return $code;
+    }
+
+    public function cleanPath($path) {
+        $path = make_relative($path, $this->baseDir);
+        $path = \str_replace(DIRECTORY_SEPARATOR, '/', $path);
+        return $path;
+    }
+
+    private function recursiveScan($path) {
+        if ($this->followSymlinks) {
+            $isDir = \is_dir($path);
+        } else {
+            $isDir = \filetype($path) === 'dir';
+        }
+        $result = array($path);
+        if ($isDir) {
+            $scan = \scandir($path);
+            $scan = \array_diff($scan, array('.', '..'));
+            foreach ($scan as $path2) {
+                $path2 = $path . DIRECTORY_SEPARATOR . $path2;
+                foreach ($this->recursiveScan($path2) as $p2) {
+                    $result[] = $p2;
+                }
+            }
+        }
+        return $result;
+    }
+
+    private function flattenInputPaths($paths) {
+        $result = array();
+        foreach ($paths as $path) {
+            $path = \realpath($path);
+            foreach ($this->recursiveScan($path) as $file) {
+                $ext = \pathinfo($file, PATHINFO_EXTENSION);
+                if ($ext === 'php' || $ext === 'hh') {
+                    $result[] = $file;
+                }
+            }
+        }
+        return $result;
+    }
 }
-
